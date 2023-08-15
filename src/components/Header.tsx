7571641106677b9c6b3f183b0292c0d64911ec72
rinehaxor <rinehaxor@users.noreply.github.com>
2023-08-16 06:19:03 +0700

    fix:outline search by title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header({ onSearch }: HeaderProps) {
             <div className="mx-[4.5px] my-[4.5px]">
                <img src={search} alt="search" className="w-[15px] h-[16px]" />
             </div>
-            <input type="text" placeholder="Cari berdasarkan judul" className="px-2 py-1 h-[13px] text-xs w-[353px]" value={searchQuery} onChange={handleSearchChange} />
+            <input type="text" placeholder="Cari berdasarkan judul" className="px-2 py-1 h-[15px] text-xs w-[353px] outline-none" value={searchQuery} onChange={handleSearchChange} />
          </div>
          <Button className="ml-[10px] w-[50px] h-[30px] bg-[#1F437A]" onClick={handleSearchClick}>
             Cari
